7451d5a4b7c53adde625a46d7ed1f3315b72fd2e
rambriz91 <rambriz91@gmail.com>
2023-01-21 21:48:11 -0800

    got question to display in js.

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,56 @@
 // variables 
-var timerEl = document.querySelector("#timer-text");
-var startBtn = document.querySelector('#start');
+var timerEl = document.getElementById('timer-text');
+var startBtn = document.getElementById('start');
+var answerBtn = document.querySelectorAll('btn');
+var answerDiv = document.getElementById('answers');
+var questionContainer = document.getElementById('question-container');
 
 var timer;
 var timerVal;
 var isWin = false;
+let shuffleQ, currentQ
 
+var questions = [
+    {
+        question: 'What primitive element determines true or false?',
+        answers: [
+            { text: 'String', correct: false },
+            { text: 'Number', correct: false },
+            { text: 'Boolean', correct: true },
+            { text: 'Bigint', correct: false },
+        ],
 
+        question: 'Which logical operator means that both conditions must be true?',
+        answers: [
+            { text: '&&', correct: true },
+            { text: '==', correct: false },
+            { text: '||', correct: false },
+            { text: '!==', correct: false },
+        ],
+
+        question: 'Which is not a language we will be learning in this course?',
+        answers: [
+            { text: 'HTML', correct: false },
+            { text: 'CSS', correct: false },
+            { text: 'Javascript', correct: false },
+            { text: 'Python', correct: true },
+        ],
+    }
+]
 // Functions 
 
 function startGame() {
     isWin = false;
+    startBtn.classList.add('hidden');
+    answerDiv.classList.remove('hidden');
+    questionContainer.classList.remove('hidden');
+    shuffleQ = questions.sort(() => Math.random() - .5)
+    currentQ = 0
     timerVal = 60;
     startBtn.disabled = true;
-    startTimer()
+    startTimer();
+    setQuestion();
+
 }
 
 function startTimer() {
@@ -21,25 +58,79 @@ function startTimer() {
         timerVal--;
         timerEl.textContent = timerVal;
         if (timerVal >= 0) {
-            // Tests if win condition is met
             if (isWin && timerVal > 0) {
-                // Clears interval and stops timer
                 clearInterval(timer);
                 winGame();
             }
         }
-        // Tests if time has run out
         if (timerVal === 0) {
-            // Clears interval
             clearInterval(timer);
             loseGame();
         }
     }, 1000);
 }
 
+function loseGame() {
+    if (timerVal === 0) {
+        alert("GAME OVER!")
+    }
+}
 
+function setQuestion() {
+    resetState()
+    showQuestion(shuffleQ[currentQ]);
+}
+
+
+function showQuestion(question) {
+    questionContainer.innerText = question.question
+    questions.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', answerQuestion)
+        answerBtn.appendchild(button)
+    })
+}
+
+
+function answerQuestion(e) {
+    var selectedAnswer = e.target
+    var correct = selectedAnswer.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerBtn.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+        currentQ++
+    }
+    else {
+        element.classlist.add('wrong');
+        x = 5;
+        timerVal - x
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+function resetState() {
+    while (answerBtn.firstChild) {
+        answerBtn.removeChild(answerBtn.firstChild)
+    }
+
+}
 
 // Event Listener 
 
-startBtn.addEventListener('click',startGame);
-
+startBtn.addEventListener('click', startGame);
